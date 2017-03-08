640ea2769dfff774bd4c66638834faa6b4519411
annivanova <ivantoaneta@mail.bg>
2017-03-08 11:49:19 +0200

    some java classes

---
+++ DemoClass.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package person;
+
+/**
+ *
+ * @author  Antoaneta Ivanova , F77618, I семестър , курс INFM002 
+ */
+public class DemoClass extends Employee{ //the class DemoClass extends Employee and I will use it to create an object and demonstrate how methods from the Employee class and the class Person that is Employee`s super class can be called here and to demonstrate how inheritance works
+      
+			
+	DemoClass(String name, int age, boolean isMan){ //the class constructor with it`s parameters
+		super(name, age, isMan, wrkHours); //I call the constructor of the super class Employee
+	}
+	public static void main(String[] args) //I need the main method in order to start the programm execution
+	{
+	DemoClass employee1= new DemoClass(name, age, isMan);{ //here I create an object from the class, that is of type DemoClass and has the name employee1
+	employee1.calculateOvertime(20, 23);	//on the object employee1 I call the methods inherited from the class Employee and give them parameters to test if they work correctly
+	employee1.showEmployee(3.75, "Alan", 23, true);
+	employee1.showPersonInfo("Alan", 23, true);  //since the class Employee extends Person I can call the method showPersonInfo in this class because this class extends Emplyee
+        
+	
+	
+	 
+	}	
+	
+	}
+}
---
+++ Person.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package person;
+
+/**
+ *
+ * @author Antoaneta Ivanova , F77618, I семестър , курс INFM002 , Софтуерни технологии в Интернет, Магистърска програма
+ */
+public class Person {  //I create a class Person and declare the following fields, the class holds information about the Person and has one method 
+ static String name; 
+ static int age;
+ static boolean isMan;
+     
+     Person(String name, int age, boolean isMan){
+         //the class has constructor with the following parameters
+        
+}
+
+public void showPersonInfo(String name, int age, boolean isMan){ // I declare a method showPersonInfo , this method has three parameters and what it does is displaying details about the person
+	
+
+System.out.println("your name is: "+ name +"\n"+"Your age is:"+ age+ "\n"+"are you a person:"+isMan+"\n");
+
+}
+
+}
+    
+
