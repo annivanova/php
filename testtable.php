4df5e1cc04134a528127dc04f02657dd2e8e3ce1
AnnIv <ivantoaneta@mail.bg>
2017-07-27 23:29:46 +0300

    Update testtable.php

--- testtable.php
+++ testtable.php
@@ -1,10 +1,10 @@
 
 <?php
-$accounts= mysql_connect("localhost", "root", "") or die(mysql_error());
+$accounts= mysql_connect("localhost", "", "") or die(mysql_error());
 $queryto=mysql_select_db("bookz", $accounts);
 
 $sql="
-INSERT INTO books (FirstName, LastName, Genre, Publisher, Title) VALUES ('Steven', 'King', 'Horror','PinguinBooks', 'Mizery');
+INSERT INTO books (FirstName, LastName, Genre, Publisher, Title) VALUES ('FName', 'SecName', 'Horror','PublisherBooks', 'BooksTitle');
 ";
 $query=mysql_query($sql, $accounts);
 
